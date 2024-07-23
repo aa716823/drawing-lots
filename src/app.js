76aeb6969629c084756025a57c49bd0bfb48f183
colton <aa716823@gmail.com>
2024-07-23 18:05:49 +0800

    新增抽籤效果、最多可選擇六人，設定預設人員

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ createApp({
         const redTeam = ref([]);
         const blueTeam = ref([]);
         let removeTimeout = null;
+        const isDrawing = ref(false);
+        const randomMember = ref('');
 
         const addMember = () => {
             if (newMember.value.trim() !== '') {
@@ -20,17 +22,35 @@ createApp({
         const selectMember = (index) => {
             if (selectedMembers.value.includes(index)) {
                 selectedMembers.value = selectedMembers.value.filter(i => i !== index);
-            } else if (selectedMembers.value.length < 4) {
+            } else if (selectedMembers.value.length < 6) {
                 selectedMembers.value.push(index);
             }
         };
 
         const drawTeams = () => {
-            const selectedNames = selectedMembers.value.map(index => members.value[index]);
-            const shuffled = selectedNames.sort(() => 0.5 - Math.random());
-            redTeam.value = shuffled.slice(0, 2);
-            blueTeam.value = shuffled.slice(2, 4);
-            selectedMembers.value = [];
+            isDrawing.value = true;
+            drawInterval = setInterval(() => {
+                redTeam.value = [
+                    members.value[Math.floor(Math.random() * members.value.length)],
+                    members.value[Math.floor(Math.random() * members.value.length)]
+                ];
+                
+                blueTeam.value = [
+                    members.value[Math.floor(Math.random() * members.value.length)],
+                    members.value[Math.floor(Math.random() * members.value.length)]
+                ]
+                // randomMember.value = members.value[Math.floor(Math.random() * members.value.length)];
+            }, 50);
+
+            setTimeout(() => {
+                clearInterval(drawInterval);
+                const selectedNames = selectedMembers.value.map(index => members.value[index]);
+                const shuffled = selectedNames.sort(() => 0.5 - Math.random());
+                redTeam.value = shuffled.slice(0, 2);
+                blueTeam.value = shuffled.slice(2, 4);
+                // selectedMembers.value = [];
+                isDrawing.value = false;
+            }, 2000);
         };
 
         const startRemoveMember = (index) => {
@@ -60,6 +80,12 @@ createApp({
             selectedMembers.value = [];
             resetTeams();
         };
+        const defaultMembers = () => {
+            members.value = ['Ann', 'Alvin', 'Boking', 'Colton', 'Eddie', '珊', '蔣爸'];
+            localStorage.removeItem('members');
+            selectedMembers.value = [];
+            resetTeams();
+        }
         return {
             newMember,
             members,
@@ -72,7 +98,10 @@ createApp({
             startRemoveMember,
             cancelRemoveMember,
             resetTeams,
-            clearMembers
+            clearMembers,
+            isDrawing,
+            randomMember,
+            defaultMembers
         };
     }
 }).mount('#app');
